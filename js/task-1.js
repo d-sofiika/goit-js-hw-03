951177075f3087961062ca299035bf6c42282f60
d-sofiika <soniadrozd2709@gmail.com>
2024-02-12 14:17:45 +0100

    my 1-2 homework

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,10 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits){
-    let sum = quantity * pricePerDroid;
-    if (sum > customerCredits) {
-        return "Insufficient funds!";
-    } else {
-       return `You ordered ${quantity} droids worth ${sum} credits!`;
-    }
+function slugify(title) {
+    const string = title.toLowerCase();
+    const slug = string.split(" ");
+    return slug.join("-");
 }
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,15 @@
-function formatMessage(message, maxLength) {
-    let length = message.length;
-    if (length <= maxLength) {
-        return message
-    } else { 
-        return (message.slice(0, maxLength)) +  "...";
-    }
+function makeArray(firstArray, secondArray, maxLength) {
+   const fullArr = firstArray.concat(secondArray);
+
+   if (fullArr.length > maxLength) {
+    return fullArr.slice(0, maxLength);
+   } else {return fullArr}
 }
-console.log(formatMessage("Curabitur ligula sapien", 16)); 
-console.log(formatMessage("Curabitur ligula sapien", 23));
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); 
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
